f684df4e5d3b2cccb9ccf9fb8b54593b1e8a6b81
Oussama EL AMRANI <oussama1elamrani@gmail.com>
2019-11-05 20:29:35 +0100

    Singleton Creational Design Pattern, One Instantiation

---
+++ src/_Singleton/SingleClass.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\_Singleton;
+
+class SingleClass
+{
+    /**
+     * @var SingleClass
+     */
+    private static $single_class = null;
+
+    public function __construct()
+    {
+        // Could not be Instantiate with [PRIVATE]
+    }
+
+    public static function getInstance()
+    {
+        if (is_null(self::$single_class))
+            self::$single_class = new SingleClass();;
+
+        return self::$single_class;
+    }
+
+}
---
+++ tests/unit/SingletonTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\unit;
+
+use App\_Factory\ShapeFactory;
+
+use PHPUnit\Framework\TestCase;
+use App\_Singleton\SingleClass;
+
+class SingletonTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_a_single_class_identified_by_hash_class()
+    {
+        $self1 = SingleClass::getInstance();
+        $self2 = SingleClass::getInstance();
+
+        $instance1 = spl_object_hash($self1);
+        $instance2 = spl_object_hash($self2);
+
+        $this->assertNotEquals($instance1, $instance2);
+
+    }
+}
